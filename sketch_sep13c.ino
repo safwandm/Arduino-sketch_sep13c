ef7a7e8e3b59186d9867aab021d2f44498ff8096
safwandm <safwan.damanik@gmail.com>
2021-10-02 10:32:25 +0700

    commit

---
+++ sketch_sep13c.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Welcome to idn boarding school");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Welcome to idn");
+  delay(1000);
+
+}
